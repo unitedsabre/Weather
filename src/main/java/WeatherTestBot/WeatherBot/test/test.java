8929f7de53b67c73084c5c884ddb4accb0c85d6c
unitedsabre <biryuchev98@mail.ru>
2023-05-27 14:47:38 +0500

    v2.0

--- src/main/java/WeatherTestBot/WeatherBot/test/test.java
+++ src/main/java/WeatherTestBot/WeatherBot/test/test.java
@@ -9,94 +9,183 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class test {
-    public static void main(String[] args) throws IOException {
-    String url = "https://www.gismeteo.ru/weather-pervouralsk-11325/weekly/";
-    Document doc = Jsoup.parse(new URL(url), 30000);
-    File file = new File("src/main/java/WeatherTestBot/WeatherBot/test/test.txt");
-    FileWriter wr = new FileWriter(file);
+    private static Pattern pattern = Pattern.compile("\\d+");
 
-    //Получение необходимых данных
-    Element tableWth = doc.select("div[class=widget-items]").first();
-    Elements table = doc.select("div[class=widget-items]");
+    private static String getDate(String stringDate) throws Exception {
+        Matcher matcher = pattern.matcher(stringDate);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        throw new Exception("Can't");
+    }
+    public static void main(String[] args) throws Exception {
+        String url = "https://www.gismeteo.ru/weather-pervouralsk-11325/weekly/";
+        Document doc = Jsoup.parse(new URL(url), 30000);
+        File file = new File("src/main/java/WeatherTestBot/WeatherBot/test/test.txt");
+        FileWriter wr = new FileWriter(file);
 
-    int i = -1;
-    String[][] weather = new String[7][8];
+        //Получение необходимых данных
+        Element tableWth = doc.select("div[class=widget-items]").first();
+        int i = -1;
+        String[][] weather = new String[7][2];
+        //i++
+        //weather[i][0] = dy;
+
+        //Получение необходимых данных
 
         Elements day = tableWth.select("div[class=widget-row widget-row-days-date]");
-        for (Element d : day) {
-            i++;
-            String dy = d.select("div[class=day]").text();
-            String dt = d.select("div[class=date]").text();
-            weather[i][0] = dt + ", " + dy;
-        }
 
+        //День
+        Elements d1 = day.select("a[class=row-item item-day-6]");
+        String dy1 = d1.select("div[class=day]").text();
+        System.out.println(dy1);
+
+        Elements d2 = day.select("a[class=row-item item-day-7]");
+        String dy2 = d2.select("div[class=day]").text();
+        System.out.println(dy2);
+
+        Elements d3 = day.select("a[class=row-item item-day-1]");
+        String dy3 = d3.select("div[class=day]").text();
+        System.out.println(dy3);
+
+        Elements d4 = day.select("a[class=row-item item-day-2]");
+        String dy4 = d4.select("div[class=day]").text();
+        System.out.println(dy4);
+
+        Elements d5 = day.select("a[class=row-item item-day-3]");
+        String dy5 = d5.select("div[class=day]").text();
+        System.out.println(dy5);
+
+        Elements d6 = day.select("a[class=row-item item-day-4]");
+        String dy6 = d6.select("div[class=day]").text();
+        System.out.println(dy6);
+
+        Elements d7 = day.select("a[class=row-item item-day-5]");
+        String dy7 = d7.select("div[class=day]").text();
+        System.out.println(dy7);
+
+        //Дата
+        Elements dD1 = day.select("a[class=row-item item-day-6]");
+        String dt1 = dD1.select("div[class=date]").text();
+        String dat1 = getDate(dt1);
+        System.out.println(dat1);
+
+        Elements dD2 = day.select("a[class=row-item item-day-7]");
+        String dt2 = dD2.select("div[class=date]").text();
+        String dat2 = getDate(dt2);
+        System.out.println(dat2);
+
+        Elements dD3 = day.select("a[class=row-item item-day-1]");
+        String dt3 = dD3.select("div[class=date]").text();
+        String dat3 = getDate(dt3);
+        System.out.println(dat3);
+
+        Elements dD4 = day.select("a[class=row-item item-day-2]");
+        String dt4 = dD4.select("div[class=date]").text();
+        String dat4 = getDate(dt4);
+        System.out.println(dat4);
+
+        Elements dD5 = day.select("a[class=row-item item-day-3]");
+        String dt5 = dD5.select("div[class=date]").text();
+        String dat5 = getDate(dt5);
+        System.out.println(dat5);
+
+        Elements dD6 = day.select("a[class=row-item item-day-4]");
+        String dt6 = dD6.select("div[class=date]").text();
+        String dat6 = getDate(dt6);
+        System.out.println(dat6);
+
+        Elements dD7 = day.select("a[class=row-item item-day-5]");
+        String dt7 = dD7.select("div[class=date]").text();
+        String dat7 = getDate(dt7);
+        System.out.println(dat7);
+
+        //Получение дня недели и числа
+
+
+        //По элементно
         Elements widget = tableWth.select("div[class=widget-row widget-row-icon]");
         Elements wD = widget.select("div[class=weather-icon tooltip]");
-        i = -1;
         for (Element w : wD) {
-            i++;
-            String wt = w.attributes().get("data-text");
-            weather[i][0] = wt;
+        String wt = w.attributes().get("data-text");
+            System.out.println(wt);
+        //zap.write(wt + "\n");
         }
+        //zap.write("Максимальная" + "\n");
 
         //Получение значения max и min температуры
+        //По элементно
         Elements TempTen = tableWth.select("div[data-row=temperature-air]");
         Elements temps = TempTen.select("div[class=values]");
         Elements maxt = temps.select("div[class=maxt]");
         Elements mint = temps.select("div[class=mint]");
-        i = -1;
         for (Element t : maxt) {
-            i++;
             String tMax = t.select("span[class=unit unit_temperature_c]").text();
-            weather[i][0] = tMax;
+            System.out.println(tMax);
+        //zap.write(tMax + " ");
         }
-        i = -1;
+        //zap.write("\n" + "Минимальная" + "\n");
+
         for (Element tm : mint) {
-            i++;
             String tMin = tm.select("span[class=unit unit_temperature_c]").text();
-            weather[i][0] = tMin;
-
+            System.out.println(tMin);
+        //zap.write(tMin + " ");
         }
+        //writer.write("\n" + "Ветер" + "\n");
+        //zap.write("\n" + "Скорость ветра, м/с" + "\n");
+
+
+
         //Получение значения скорости ветра
-        Elements wind = table.select("div[data-row=wind-speed]");
-        i = -1;
-        for (Element windT : wind) {
-            i++;
+        //По элементно
+        Elements wind = tableWth.select("div[data-row=wind-speed]");
+        Elements wid = wind.select("div[class=row-item]");
+        for (Element windT : wid) {
             String winds = windT.select("span[class=wind-unit unit unit_wind_m_s]").text();
-            weather[i][0] = winds;
+            System.out.println(winds);
+            //zap.write(winds);
+        }
+            //zap.write("\n");
 
-        }
+
         //Получение значения направления ветра
-        Elements windD = table.select("div[data-row=wind-direction]");
-        i = -1;
-        for (Element ignored : windD) {
-            i++;
-            String windDirection = windD.select("div[class=direction]").text();
-            weather[i][0] = windDirection;
+        //По элементно
+        Elements windD = tableWth.select("div[data-row=wind-direction]");
+        Elements winD = windD.select("div[class=row-item]");
+        for (Element windR : winD) {
+            String windDirection = windR.select("div[class=direction]").text();
+            System.out.println(windDirection);
+        //zap.write("Направление ветра" + "\n");
+        //zap.write(windDirection);
         }
+        //zap.write("\n" + "Давление, мм рт. ст." + "\n");
+        //zap.write("Максимальное" + "\n");
 
 
         //Получение значения max и min давления
+        //По элементно
         Elements pressureTen = tableWth.select("div[data-row=pressure]");
         Elements pressure = pressureTen.select("div[class=value style_size_m]");
         Elements davlMax = pressure.select("div[class=maxt]");
         Elements davlMin = pressure.select("div[class=mint]");
-        i = -1;
         for (Element pressureMax : davlMax) {
-            i++;
             String prMax = pressureMax.select("span[class=unit unit_pressure_mm_hg_atm]").text();
-            weather[i][0] = prMax;
+            System.out.println(prMax);
+        //zap.write(prMax + " ");
         }
+        //zap.write("\n" + "Минимальное" + "\n");
 
-        i = -1;
         for (Element pressureMin : davlMin) {
-            i++;
             String prMin = pressureMin.select("span[class=unit unit_pressure_mm_hg_atm]").text();
-            weather[i][0] = prMin;
+            System.out.println(prMin);
+        //zap.write(prMin + " ");
         }
-        wr.close();
-        System.out.println(weather);
+        //zap.close();
+        //System.out.println(weather);
+
 }
 }
